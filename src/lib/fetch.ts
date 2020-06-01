5f540110452ee122d261b127f73981492a20b896
Jacob Wan <jacobwan840@gmail.com>
2020-06-01 15:02:43 -0700

    Accumulated changes 2020-06-01

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,7 +1,40 @@
-import { FetchBase } from "./fetchBase"
+import { TestBase } from "./testBase"
+import { ResourceTimingEntry } from "../@types"
+import { asyncGetEntry, normalizeEntry } from "./resourceTiming"
 
-export default class Fetch extends FetchBase {
+export default class Fetch extends TestBase {
     makeBeaconURL(): string {
         return this.provider.makeFetchBeaconURL(this.config)
+    }
+
+    getResourceUrl(): string {
+        return this.provider.getResourceUrl(this.config)
+    }
+
+    makeTestSteps(): Promise<unknown[]> {
+        return Promise.all<unknown>([
+            this.test(),
+            this.provider.makeClientInfoPromise(this),
+        ])
+    }
+
+    test(): Promise<ResourceTimingEntry> {
+        return Promise.all<string, ResourceTimingEntry>([
+            this.fetchObjectAndId(),
+            asyncGetEntry(this.getResourceUrl(), 5000)
+        ])
+            .then(
+                ([id, entry]): ResourceTimingEntry => {
+                    const timing = normalizeEntry(entry)
+                    return this.provider.createFetchResult(timing, id, this.config)
+                }
+            )
+    }
+
+    fetchObjectAndId(): Promise<string> {
+        return fetch(this.getResourceUrl())
+            .then((res): string => {
+                return res.headers.get("X-Datacenter") || ""
+            })
     }
 }
--- src/lib/fetchThroughput.ts
+++
@@ -1,9 +0,0 @@
-import { FetchBase } from "./fetchBase"
-import { ResourceTimingEntry } from "../@types"
-
-export default class FetchThroughput extends FetchBase {
-    makeBeaconURL(): string {
-        debugger
-        throw new Error("FetchThroughput.makeBeaconURL not implemented.")
-    }
-}
