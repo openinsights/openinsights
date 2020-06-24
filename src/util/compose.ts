029c5414c6b127bc635a8ae24491ab7762aab90f
Jacob Wan <jacobwan840@gmail.com>
2020-06-24 10:39:25 -0700

    Docblock for compose

--- src/util/compose.ts
+++ src/util/compose.ts
@@ -4,9 +4,12 @@
 */
 
 /**
- * TODO
- * @param fn1 TODO
- * @param fns TODO
+ * Creates a pipeline that can be used to execute a series of discrete
+ * functions on an object of the given type R, returning the final result.
+ * Functions are executed in the reverse order given.
+ *
+ * @param fn1 Last function in the pipeline
+ * @param fns Pipeline functions, minus the last one.
  */
 const compose = <R>(fn1: (a: R) => R, ...fns: ((a: R) => R)[]) =>
     fns.reduce((prevFn, nextFn) => (value) => prevFn(nextFn(value)), fn1)
