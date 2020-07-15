49d82942387c140091b53433db9507140c370f0b
Jacob Wan <jacobwan840@gmail.com>
2020-07-15 10:26:19 -0700

    Update src/lib/getNetworkInformation.ts
    Co-authored-by: Patrick Hamann <patrick@fastly.com>

--- src/lib/getNetworkInformation.ts
+++ src/lib/getNetworkInformation.ts
@@ -6,7 +6,7 @@ import { Navigator, NetworkInformation } from "../@types"
  */
 export default function getNetworkInformation(): NetworkInformation {
     const navWithInfo = navigator as Navigator
-    const connection = navWithInfo && navWithInfo.connection
+    const connection = navWithInfo?.connection
     if (connection) {
         const res: NetworkInformation = {}
         /* eslint-disable guard-for-in */
