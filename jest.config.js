4b4b97f1173415af837c6e6353e0a89048e06bd9
Jacob Wan <jacobwan840@gmail.com>
2022-07-21 08:46:17 -0700

    Add jest-testenvironment-jsdom to get tests running

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: "ts-jest",
+    testEnvironment: "jsdom",
     testRegex: "\\.test\\.ts",
     collectCoverageFrom: ["src/index.ts", "src/lib/*.ts", "src/util/*.ts"],
     automock: false,
